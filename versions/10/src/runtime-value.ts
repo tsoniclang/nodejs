7a2609031b0417a35518aa8df637781c192da264
Jeswin <jeswinpk@agilehead.com>
2026-05-08 09:12:38 +0530

    Close node runtime value carriers

--- versions/10/src/runtime-value.ts
+++ versions/10/src/runtime-value.ts
@@ -1,1 +1,14 @@
-export type RuntimeValue = string | number | boolean | object | null | undefined;
+export interface RuntimeObject {
+  [key: string]: RuntimeValue;
+}
+
+export type RuntimeArray = RuntimeValue[];
+
+export type RuntimeValue =
+  | string
+  | number
+  | boolean
+  | RuntimeArray
+  | RuntimeObject
+  | null
+  | undefined;
